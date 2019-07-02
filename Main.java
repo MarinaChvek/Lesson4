2f54ec0d70cf4472f4007cafc19f484b165ac5fa
MarinaChvek <52216049+MarinaChvek@users.noreply.github.com>
2019-07-02 21:51:56 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[12];
+
+        for (int i = 0; i < 12; i++) {
+            array[i] = i+1;
+            System.out.print(array[i] + " ");
+            // write your code here
+        }
+    }
+}
